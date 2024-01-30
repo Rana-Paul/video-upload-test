14dd9b8d695fa4182698e4489086a0953a1f69d8
Rana-Mapscrew <ranapaul741223@gmail.com>
2024-01-30 19:49:06 +0530

    next auth completed

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,36 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+export const authOptions: NextAuthOptions = {
+  // Secret for Next-auth, without this JWT encryption/decryption won't work
+  secret: process.env.NEXTAUTH_SECRET,
+
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.user.accessToken = token.accessToken as string;
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+export {handler as GET, handler as POST}
